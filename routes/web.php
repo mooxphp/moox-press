9f6d012455c15a99550da7a27c61d237c63a220d
Alf Drollinger <alf@drollinger.info>
2024-03-20 14:09:51 +0100

    WpUser working CRUD

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/wp');
 });
+
+Route::any('{any}', function ($any) {
+    if (! str_contains(request()->path(), '/wp/')) {
+        return redirect('/wp/'.ltrim(request()->path(), '/'));
+    }
+})->where('any', '.*');
