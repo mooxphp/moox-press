e2c7d5f23a166f9243ade92a8e2e1c3e2ef23712
KimSpeer <93331309+Kim-the-Diamond@users.noreply.github.com>
2024-03-21 11:41:27 +0100

    wip

--- symlink.sh
+++ symlink.sh
@@ -5,9 +5,8 @@ ln -s ../../vendor/roots/wordpress-full/wp-includes ./public/wp/wp-includes
 ln -s ../../vendor/roots/wordpress-full/index.php ./public/wp/index.php
 ln -s ../../vendor/roots/wordpress-full/wp-activate.php ./public/wp/wp-activate.php
 ln -s ../../vendor/roots/wordpress-full/wp-blog-header.php ./public/wp/wp-blog-header.php
-ln -s ../../vendor/roots/wordpress-full/wp-comments-posts.php ./public/wp/wp-comments-posts.php
+ln -s ../../vendor/roots/wordpress-full/wp-comments-post.php ./public/wp/wp-comments-post.php
 ln -s ../../vendor/roots/wordpress-full/wp-config-sample.php ./public/wp/wp-config-sample.php
-ln -s ../../vendor/roots/wordpress-full/wp-comments-posts.php ./public/wp/wp-comments-posts.php
 ln -s ../../vendor/roots/wordpress-full/wp-cron.php ./public/wp/wp-cron.php
 ln -s ../../vendor/roots/wordpress-full/wp-links-opml.php ./public/wp/wp-links-opml.php
 ln -s ../../vendor/roots/wordpress-full/wp-load.php ./public/wp/wp-load.php
@@ -18,16 +17,15 @@ ln -s ../../vendor/roots/wordpress-full/wp-signup.php ./public/wp/wp-signup.php
 ln -s ../../vendor/roots/wordpress-full/wp-trackback.php ./public/wp/wp-trackback.php
 ln -s ../../vendor/roots/wordpress-full/xmlrpc.php ./public/wp/xmlrpc.php
 
-mkdir ./public/wp/wp-content
-mkdir ./public/wp/wp-content/themes
-ln -s ../../vendor/roots/wordpress-full/wp-content/themes/twentytwentyfour ./public/wp/wp-content/themes/twentytwentyfour
+mkdir -p ./public/wp/wp-content/themes
+ln -s ../../../../vendor/roots/wordpress-full/wp-content/themes/twentytwentyfour ./public/wp/wp-content/themes/twentytwentyfour
 
 mkdir ./public/wp/wp-content/plugins
-ln -s ../../vendor/roots/wordpress-full/wp-content/plugins/hello.php ./public/wp/wp-content/plugins/hello.php
-ln -s ../../vendor/roots/wordpress-full/wp-content/plugins/akismet ./public/wp/wp-content/plugins/akismet
+ln -s ../../../../vendor/roots/wordpress-full/wp-content/plugins/hello.php ./public/wp/wp-content/plugins/hello.php
+ln -s ../../../../vendor/roots/wordpress-full/wp-content/plugins/akismet ./public/wp/wp-content/plugins/akismet
 
 mkdir ./storage/app/wp-uploads
-ln -s ./storage/app/wp-uploads ./public/wp/wp-content/uploads
+ln -s ../../../storage/app/wp-uploads ./public/wp/wp-content/uploads
 
 mkdir ./storage/app/wp-languages
-ln -s ./storage/app/wp-languages ./public/wp/wp-content/languages
+ln -s ../../../storage/app/wp-languages ./public/wp/wp-content/languages
