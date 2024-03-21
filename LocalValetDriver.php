8913c380a056946afb7761da6f4cd4353a72c219
Alf Drollinger <alf@drollinger.info>
2024-03-21 11:14:50 +0100

    Valet w-i-p

---
+++ LocalValetDriver.php
@@ -0,0 +1,35 @@
+<?php
+
+use Valet\Drivers\LaravelValetDriver;
+
+class LocalValetDriver extends LaravelValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     */
+    public function serves(string $sitePath, string $siteName, string $uri): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     */
+    public function frontControllerPath(string $sitePath, string $siteName, string $uri): string
+    {
+
+        // WordPress admin or includes
+        if (preg_match('#/wp/(wp-admin|wp-includes)#', $uri)) {
+            return $sitePath.'/public/wp/index.php';
+        }
+
+        if (str_contains($uri, '/wp')) {
+            // Handle WordPress front-end
+            return $sitePath.'/public/wp/index.php';
+        }
+
+        // Fallback to Laravel's front controller
+        return parent::frontControllerPath($sitePath, $siteName, $uri);
+
+    }
+}
