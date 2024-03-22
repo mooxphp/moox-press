ba12805cb5117c51fff77c62098dfeaa123263b0
Alf Drollinger <alf@drollinger.info>
2024-03-22 23:26:19 +0100

    Update LocalValetDriver.php

--- LocalValetDriver.php
+++ LocalValetDriver.php
@@ -17,24 +17,27 @@ class LocalValetDriver extends LaravelValetDriver
      */
     public function frontControllerPath(string $sitePath, string $siteName, string $uri): ?string
     {
-        if (str_contains($uri, '/wp/') || str_ends_with($uri, '/wp')) {
+        $public = '/public';
+        $wpslug = '/wp';
+
+        if (str_contains($uri, $wpslug.'/') || str_ends_with($uri, $wpslug)) {
 
             if (str_contains($uri, '/wp-admin')) {
 
                 if (str_ends_with($uri, '/wp-admin/') || str_ends_with($uri, '/wp-admin') || str_ends_with($uri, '/wp-admin/index.php')) {
-                    return $sitePath.'/public/wp/wp-admin/index.php';
+                    return $sitePath.$public.$wpslug.'/wp-admin/index.php';
                 }
 
-                return $sitePath.'/public'.$uri;
+                return $sitePath.$public.$uri;
             }
 
             if (str_contains($uri, 'wp-login.php')) {
-                return $sitePath.'/public/wp/wp-login.php';
+                return $sitePath.$public.$wpslug.'/wp-login.php';
             }
 
-            return $sitePath.'/public/wp/index.php';
+            return $sitePath.$public.$wpslug.'/index.php';
         }
 
-        return $sitePath.'/public/index.php';
+        return $sitePath.$public.'/index.php';
     }
 }
