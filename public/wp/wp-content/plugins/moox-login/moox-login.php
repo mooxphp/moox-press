6f857850ed11cb3ef14fc1fb1a5d2ff8f6d0302d
Alf Drollinger <alf@drollinger.info>
2024-03-24 08:08:30 +0100

    SSO w-i-p

--- public/wp/wp-content/plugins/moox-login/moox-login.php
+++ public/wp/wp-content/plugins/moox-login/moox-login.php
@@ -1,10 +1,56 @@
 <?php
 /*
 Plugin Name: Moox Press
-Description: Plugin to redirect WordPress login attempts to Laravel
+Description: Plugin for integrating WordPress with Laravel
 Author: Moox
 Version: 0.1
 */
+
+// Token-based authentication
+add_action('init', function () {
+    if (isset($_GET['auth_token'])) {
+        $token = $_GET['auth_token'];
+
+        $user_id = $token; // Implement a function to validate
+
+        if ($user_id) {
+            wp_clear_auth_cookie();
+            wp_set_auth_cookie($user_id);
+
+            wp_redirect(admin_url());
+            exit;
+        }
+    }
+});
+
+// API-based authentication
+add_action('rest_api_init', function () {
+    register_rest_route('custom/v1', '/login/', [
+        'methods' => 'POST',
+        'callback' => 'custom_login',
+        'permission_callback' => '__return_true',
+    ]);
+});
+
+function custom_login(WP_REST_Request $request)
+{
+    $creds = [
+        'user_login' => $request->get_param('username'),
+        'user_password' => $request->get_param('password'),
+        'remember' => true,
+    ];
+
+    $user = wp_signon($creds, false);
+
+    if (is_wp_error($user)) {
+        return new WP_REST_Response($user->get_error_message(), 403);
+    }
+
+    wp_set_current_user($user->ID);
+    wp_set_auth_cookie($user->ID, true);
+
+    return new WP_REST_Response('Logged in', 200);
+}
 
 // Redirect WordPress login attempts to Laravel
 add_action('init', function () {
