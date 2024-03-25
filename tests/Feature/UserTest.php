c4666ea8e745b706551513667a2c7cf9135cb665
Kim-the-Diamond <Kim-the-Diamond@users.noreply.github.com>
2024-03-25 08:15:00 +0000

    Fix styling

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,7 +6,7 @@ use Moox\Press\Models\WpUser;
 use Moox\Press\Resources\WpUserResource;
 
 beforeEach(function () {
-    if(!Schema::hasTable('wp_users')){
+    if (! Schema::hasTable('wp_users')) {
         $sqlFilePath = 'moox-press.sql';
         $sql = file_get_contents($sqlFilePath);
         DB::unprepared($sql);
