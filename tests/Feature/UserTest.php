8710646fda66fc01915f8e3cf3219159eba36345
KimSpeer <93331309+Kim-the-Diamond@users.noreply.github.com>
2024-03-25 09:10:42 +0100

    Update UserTest.php

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -33,6 +33,6 @@ it('can get WpUserResource', function () {
     $this->actingAs($this->user)->get(WPUserResource::getUrl('index'))->assertSuccessful();
 });
 
-it('cant get WpUserResource', function () {
+test('Unauthorized cant get WpUserResource', function () {
     $this->get(WPUserResource::getUrl('index'))->assertRedirect('admin/login');
 });
