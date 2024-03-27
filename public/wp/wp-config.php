6ce12061f69c311b20d9cd1fa659fea5200defb7
Alf Drollinger <alf@drollinger.info>
2024-03-27 15:47:27 +0100

    Moved all crypt stuff to .env

--- public/wp/wp-config.php
+++ public/wp/wp-config.php
@@ -17,14 +17,14 @@ define('DB_COLLATE', '');
 define('WP_SITEURL', $_ENV['APP_URL'].$_ENV['WP_SLUG']);
 define('WP_HOME', $_ENV['APP_URL'].$_ENV['WP_SLUG']);
 
-define('AUTH_KEY', '^a1KZcj]@K o)qGe&WRFaPzC*7^o&+P{Zc:.T{1b^>lM[`7hy:]E5G)w*(z.z^U.');
-define('SECURE_AUTH_KEY', '@jJ6x98 )r,;E)N82%<iy-JJ0!9h7Fj1zoVQi]uZvDqSh!k8,CV=?(HdoDZ</v*6');
-define('LOGGED_IN_KEY', 't#I^q3&j~D7N7uUHq7|`PPUs#CALNxX]zNE:aVm97[4aVZ(!g#!<H?r@:XL?Y:*O');
-define('NONCE_KEY', 'K4+OB$sVp;V?&U1F&_c+`z&gk~%Df%S.iF9c$8uC>&1$-vTJYOPg$]!?79;~Z/.B');
-define('AUTH_SALT', 'R?TLb5RUbP~PFU2K]Z5fPZ/t1U[}S/y-Z$<:yt0Jh22.g<*-nlvdKR,uH*H{g2%8');
-define('SECURE_AUTH_SALT', 'BK-U?`A+#rRHl8qjHX]nTfpqBiB2|C=Q[{XIo??_&W]qo&#cB-g-6&`Ja8yo;mIk');
-define('LOGGED_IN_SALT', '1pMhb?2TdA+8]skBU2V:O]Q Eusi#<U6oL7z7P_azHMVc,=bnQU,`8z)a=IgK().');
-define('NONCE_SALT', '5&;n)9X]tvr9i0F-z7 :xku[F5aESzVL`Wb#qS6{qo@WyRy98:ge[gQR6b4;q?7O');
+define('AUTH_KEY', $_ENV['WP_AUTH_KEY']);
+define('SECURE_AUTH_KEY', $_ENV['WP_SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $_ENV['WP_LOGGED_IN_KEY']);
+define('NONCE_KEY', $_ENV['WP_NONCE_KEY']);
+define('AUTH_SALT', $_ENV['WP_AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_ENV['WP_SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT', $_ENV['WP_LOGGED_IN_SALT']);
+define('NONCE_SALT', $_ENV['WP_NONCE_SALT']);
 
 define('MOOX_HASH', $_ENV['APP_KEY']);
 
