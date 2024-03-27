3c0fa21a5a4d945d845c30871b077cf909b53a59
Alf Drollinger <alf@drollinger.info>
2024-03-27 09:38:57 +0100

    Working auth-flow, not crypted

--- public/wp/wp-content/plugins/moox-press/moox-press.php
+++ public/wp/wp-content/plugins/moox-press/moox-press.php
@@ -6,12 +6,51 @@ Author: Moox
 Version: 0.1
 */
 
-// Token-based authentication
-add_action('init', function () {
+// Get config
+if (defined('MOOX_HASH')) {
+    $mooxHash = MOOX_HASH;
+}
+
+if (defined('LOCK_WP')) {
+    $lockWp = LOCK_WP;
+}
+
+if (defined('HIDE_LOGIN')) {
+    $hideLogin = HIDE_LOGIN;
+}
+
+if (defined('FORGOT_PASSWORD')) {
+    $forgotPassword = FORGOT_PASSWORD;
+}
+
+if (defined('ENABLE_MFA')) {
+    $enableMfa = ENABLE_MFA;
+}
+
+if (defined('REGISTRATION')) {
+    $registration = REGISTRATION;
+}
+
+if (defined('WP_DEBUG')) {
+    $wpDebug = WP_DEBUG;
+}
+
+function moox_lock_wp_frontend()
+{
+    if (defined('LOCK_WP') && LOCK_WP) {
+        if (! is_user_logged_in() && $GLOBALS['pagenow'] !== 'wp-login.php') {
+            auth_redirect();
+        }
+    }
+}
+add_action('template_redirect', 'moox_lock_wp_frontend');
+
+function moox_auth_token()
+{
     if (isset($_GET['auth_token'])) {
         $token = $_GET['auth_token'];
-
-        $user_id = $token; // Implement a function to validate
+        // TODO - Implement a function to validate the token and get the user ID
+        $user_id = $token;
 
         if ($user_id) {
             wp_clear_auth_cookie();
@@ -21,25 +60,28 @@ add_action('init', function () {
             exit;
         }
     }
-});
+}
+add_action('init', 'moox_auth_token');
 
-// After WordPress logout, redirect to Laravel logout
-add_action('init', function () {
+function moox_redirect_logout()
+{
     $url = strtok($_SERVER['REQUEST_URI'], '?');
 
-    if (str_ends_with($url, 'wp-login.php') && $_GET['action'] === 'logout') {
+    if (str_ends_with($url, 'wp-login.php') && isset($_GET['action']) && $_GET['action'] === 'logout') {
         wp_logout();
         wp_redirect('https://'.$_SERVER['SERVER_NAME'].'/admin/logout');
         exit;
     }
-});
+}
+add_action('init', 'moox_redirect_logout');
 
-// Redirect WordPress login attempts to Laravel
-add_action('init', function () {
+function moox_redirect_login()
+{
     $url = strtok($_SERVER['REQUEST_URI'], '?');
 
     if (str_ends_with($url, 'wp-login.php') && ! isset($_GET['action'])) {
         wp_redirect('https://'.$_SERVER['SERVER_NAME'].'/admin/login');
         exit;
     }
-});
+}
+add_action('init', 'moox_redirect_login');
