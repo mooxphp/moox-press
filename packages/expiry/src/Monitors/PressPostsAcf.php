daa51789248c21a3d07a9da6ed0402e5c9b11011
Alf Drollinger <alf@drollinger.info>
2024-04-02 11:05:26 +0200

    Update PressPostsAcf.php

--- packages/expiry/src/Monitors/PressPostsAcf.php
+++ packages/expiry/src/Monitors/PressPostsAcf.php
@@ -4,8 +4,43 @@ namespace Moox\Expiry\Monitors;
 
 class PressPostsAcf
 {
-    public function monitor()
+    public function __construct()
     {
-        // Monitor the Press Posts ACF
+        // Construct the Press Posts ACF
+    }
+
+    public function __invoke()
+    {
+        // Invoke the Press Posts ACF
+    }
+
+    public function boot()
+    {
+        // Boot the Press Posts ACF
+    }
+
+    public function monitors()
+    {
+        $model = \Moox\Press\Models\WpPost::class;
+        $query = $model::query();
+
+        $query->where('post_type', 'post');
+
+        return $query->get();
+    }
+
+    public function executes()
+    {
+        // Execute the Press Posts ACF
+    }
+
+    public function notifies()
+    {
+        // Notify the Press Posts ACF
+    }
+
+    public function escalates()
+    {
+        // Escalate the Press Posts ACF
     }
 }
