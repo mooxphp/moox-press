25a7512f2193c987b3bf73aaa846225f13be5fc7
Alf Drollinger <alf@drollinger.info>
2024-06-07 06:30:49 +0200

    Update ListExpiries.php

--- packages/expiry/src/Resources/ExpiryResource/Pages/ListExpiries.php
+++ packages/expiry/src/Resources/ExpiryResource/Pages/ListExpiries.php
@@ -2,16 +2,10 @@
 
 namespace Moox\Expiry\Resources\ExpiryResource\Pages;
 
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Moox\Expiry\Resources\ExpiryResource;
 
 class ListExpiries extends ListRecords
 {
     protected static string $resource = ExpiryResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [CreateAction::make()];
-    }
 }
