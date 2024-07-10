824c559200a50bf11dba9b4076003517029d40fb
Kim-the-Diamond <Kim-the-Diamond@users.noreply.github.com>
2024-07-10 10:51:04 +0000

    Fix styling

--- packages/expiry/routes/api.php
+++ packages/expiry/routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Moox\Expiry\Http\Controllers\Api\ExpiryController;
 
 if (config('expiry.api')) {
-    Route::prefix('/api/expiries')->group(function(){
-        Route::get('count',[ExpiryController::class, 'count']);
-        Route::get('count/user/{user}',[ExpiryController::class, 'countForUser']);
+    Route::prefix('/api/expiries')->group(function () {
+        Route::get('count', [ExpiryController::class, 'count']);
+        Route::get('count/user/{user}', [ExpiryController::class, 'countForUser']);
         Route::apiResource('/', ExpiryController::class);
     });
 }
