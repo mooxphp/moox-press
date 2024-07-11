38e159064f99c7839c0e7be8b3001547e136544d
AzGasim <AzGasim@users.noreply.github.com>
2024-07-11 07:31:40 +0000

    Fix styling

--- packages/expiry/src/Resources/ExpiryResource.php
+++ packages/expiry/src/Resources/ExpiryResource.php
@@ -2,19 +2,19 @@
 
 namespace Moox\Expiry\Resources;
 
-use Filament\Tables;
-use Filament\Forms\Form;
-use Filament\Tables\Table;
-use Moox\Expiry\Models\Expiry;
-use Filament\Resources\Resource;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Section;
-use Filament\Tables\Actions\ViewAction;
 use Filament\Forms\Components\TextInput;
-use Filament\Tables\Filters\SelectFilter;
-use Illuminate\Database\Eloquent\Builder;
-use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
 use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Moox\Expiry\Models\Expiry;
 use Moox\Expiry\Resources\ExpiryResource\Pages;
 
 class ExpiryResource extends Resource
@@ -188,8 +188,8 @@ class ExpiryResource extends Resource
                     ->searchable()
                     ->sortable(query: function (Builder $query, string $direction): Builder {
                         return $query->leftJoin('tyar9_users', 'expiries.notified_to', '=', 'tyar9_users.ID')
-                                     ->orderBy('tyar9_users.display_name', $direction)
-                                     ->select('expiries.*');
+                            ->orderBy('tyar9_users.display_name', $direction)
+                            ->select('expiries.*');
                     })
                     ->limit(50),
                 Tables\Columns\TextColumn::make('expiry_job')
